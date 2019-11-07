8c94a5ff6b42cd48743bdca0660d7236be8e5fc9
Purushotham <pryadavalli@gmail.com>
2019-11-06 22:46:49 -0500

    new changes

--- my-client/src/app/login/login.component.ts
+++ my-client/src/app/login/login.component.ts
@@ -9,15 +9,32 @@ export class LoginComponent implements OnInit {
 
   public hasError = false;
   public isSpecial = true;
+  public greeting ="";
+  public  loginname;
+  public password;
   public messageClasses ={
     "text-success":!this.hasError,
-    "text-danger": this.hasError,
-    "text-special": this.isSpecial
+    "text-danger":  this.hasError,
+    "text-special": this.hasError
   }
+ 
   constructor() { }
 
   ngOnInit() {
     
   }
 
+  Login(event)
+  {
+    if(this.loginname == "test" && this.password=="test")
+    {
+      this.greeting = "Welcome to angular world!";
+      this.hasError = false;
+    }
+    else
+    {
+      this.hasError = true;
+    }
+
+  }
 }
