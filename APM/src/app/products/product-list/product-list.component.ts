b56219c8630efa56a62e12574a28fb02fda1f623
Purushotham <pryadavalli@gmail.com>
2019-12-22 02:01:33 -0500

    apm

---
+++ APM/src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,105 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+import { Store, select } from '@ngrx/store';
+import * as fromProduct from '../state/product.reducer';
+import * as productActions from '../state/product.actions';
+import { takeWhile } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'pm-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit, OnDestroy {
+
+  componentActive = true;
+
+  pageTitle = 'Products';
+  errorMessage: string;
+
+  displayCode: boolean;
+
+  products: Product[];
+
+  // Used to highlight the selected product in the list
+  selectedProduct: Product | null;
+  errorMessage$: Observable<string>;
+
+
+  constructor(private store: Store<fromProduct.State>, private productService: ProductService) { }
+
+  ngOnInit(): void {
+
+    // this.sub = this.productService.selectedProductChanges$.subscribe(
+    //   selectedProduct => this.selectedProduct = selectedProduct
+    // );
+
+    this.store.dispatch(new productActions.Load());
+    
+    this.store.pipe(select(fromProduct.getProducts),
+    takeWhile(()=> this.componentActive)
+    ).subscribe(
+      (products:Product[]) => this.products = products
+    );
+
+
+    this.store.pipe(select(fromProduct.getCurrentProduct)).subscribe(
+      currentProduct => this.selectedProduct = currentProduct
+    );
+
+    this.errorMessage$ = this.store.pipe(select(fromProduct.getError));
+
+    // this.productService.getProducts().subscribe({
+    //   next: (products: Product[]) => this.products = products,
+    //   error: (err: any) => this.errorMessage = err.error
+    // });
+
+    //** below one is without selector implementation */
+    // this.store.pipe(select('products')).subscribe(
+    //   products=> {
+    //     if(products)
+    //       this.displayCode = products.showProductCode
+    //   }
+    // )
+
+    // * Below code is with selector implementation in the reducer
+    this.store.pipe(select(fromProduct.getShowProductCode)).subscribe(
+      showProductCode => this.displayCode = showProductCode
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.componentActive = false;
+  }
+
+  checkChanged(value: boolean): void {
+
+
+    // this.store.dispatch({
+    //   type:'TOGGLE_PRODUCT_CODE',
+    //   payload: value
+    // });
+
+    this.store.dispatch(
+      new productActions.ToggleProductCode(value)
+    )
+  }
+
+  newProduct(): void {
+    //this.productService.changeSelectedProduct(this.productService.newProduct());
+
+    this.store.dispatch(new productActions.InitializeCurrentProduct());
+  }
+
+  productSelected(product: Product): void {
+    //this.productService.changeSelectedProduct(product);
+
+    this.store.dispatch(
+      new productActions.SetCurrentProduct(product)
+    );
+
+  }
+
+}
---
+++ APM/src/app/products/state/product.effects.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+
+import * as productActions from './product.actions';
+import { mergeMap, map, catchError } from 'rxjs/operators';
+import { ProductService } from '../product.service';
+import { Product } from '../product';
+import { of, Observable } from 'rxjs';
+import { Action } from '@ngrx/store';
+
+@Injectable()
+export class ProductEffects {
+
+    constructor(private actions$: Actions, private productService: ProductService) {
+
+    }
+    @Effect()
+    loadProducts$: Observable<Action> = this.actions$.pipe(
+        ofType(productActions.ProductActionTypes.Load),
+        mergeMap((action: productActions.Load) => this.productService.getProducts().pipe(
+            map((products: Product[]) => (new productActions.LoadSuccess(products))),
+            catchError(err => of(new productActions.LoadFail(err)))
+        )
+        )
+    )
+
+    @Effect()
+    updateProduct$: Observable<Action> = this.actions$.pipe(
+        ofType(productActions.ProductActionTypes.UpdateProduct),
+        map((action:productActions.UpdateProduct)=>action.payload),
+        mergeMap((product: Product) => this.productService.updateProduct(product).pipe(
+            map(updateProduct => (new productActions.UpdateProductSuccess(updateProduct))),
+            catchError(err => of(new productActions.UpdateProductFail(err)))
+        )
+        )
+    )
+}
